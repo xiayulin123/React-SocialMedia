1a170953205ad266e9abea9b29f8e125cedbfbf3
xiayulin123 <xiayulinsky@gmail.com>
2023-12-30 12:38:31 -0500

    check switched to ubuntu

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ const App = () => {
             <Router path="/sign-in" element={<SigninForm/> } />
             {/* {private routes} */}
             <Route index element= {<Home/>} />
+            
         </Routes>
     </main>
   )
