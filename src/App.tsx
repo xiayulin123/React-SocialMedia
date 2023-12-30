e20f9b53d55ae2007bdd80ebea381a5d31f74225
Yulin Xia <yulinxia@Yulins-MacBook-Air.local>
2023-12-30 12:29:27 -0500

    switch to ubuntu

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-
+import './globals.css';
+import {Routes, Route, Router} from 'react-router-dom'
 const App = () => {
   return (
-    <div>App</div>
+    <main className='flex h-screen '>
+        <Routes>
+            {/* {public routes} */}
+            <Router path="/sign-in" element={<SigninForm/> } />
+            {/* {private routes} */}
+            <Route index element= {<Home/>} />
+        </Routes>
+    </main>
   )
 }
 
