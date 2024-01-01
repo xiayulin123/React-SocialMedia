ad9e38fae43928ffb17a25bf319faa248b12403c
xiayulin123 <xiayulinsky@gmail.com>
2024-01-01 18:12:04 -0500

    fixed error with env file

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -1,15 +1,20 @@
-import {Client, Account, Databases, Storage, Avatars} from 'appwrite'
+import {Client, Account, Databases, Storage, Avatars} from 'appwrite';
+
 
 export const appwriteConfig = {
-    projectId: import.meta.env.APPWRITE_ID,
-    url: import.meta.env.APPWRITE_URL,
-
+    projectId: import.meta.env.VITE_APPWRITE_ID,
+    url: import.meta.env.VITE_APPWRITE_URL,
+    databaseId: import.meta.env.VITE_APPWRITE_DATABASE,
+    postId: import.meta.env.VITE_APPWRITE_POSTS,
+    storageId: import.meta.env.VITE_APPWRITE_STORAGE,
+    saveId: import.meta.env.VITE_APPWRITE_SAVES,
+    userCollectionId: import.meta.env.VITE_APPWRITE_USERS,
 }
-
+console.log("here is the id:     ",appwriteConfig.projectId)
 export const client = new Client();
 
-client.setProject(appwriteConfig.projectId);
-client.setEndpoint('https://cloud.appwrite.io/v1');
+client.setProject(appwriteConfig.projectId!);
+client.setEndpoint(appwriteConfig.url!);
 
 export const account = new Account(client);
 export const databases = new Databases(client);
