4d9d7998082f658ed3665c9d4efe10c67c0897ee
xiayulin123 <xiayulinsky@gmail.com>
2024-01-01 16:48:08 -0500

    updating

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,11 @@
+import * as z from "zod"
+
+export const SignUpValidation = z.object({
+    name: z.string().min(2, {message: 'Too Short'}),
+    username: z.string().min(2,{message: 'Too Short'}).max(50,{message: 'Too Long'}),
+    email: z.string().email(),
+    password: z.string().min(8, {message: 'Password must be at least 8 characters'}),
+
+
+  })
+  
