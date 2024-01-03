2261a384271126b2912c7e7f61cfc36c01cb2499
xiayulin123 <xiayulinsky@gmail.com>
2024-01-03 14:55:39 -0500

    root page layout

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -2,8 +2,10 @@ import React, { useEffect } from 'react'
 import { Link, useNavigate} from 'react-router-dom'
 import { Button } from '../ui/button'
 import { useSignOutAccount } from '@/lib/react-query/queriesAndMutations'
+import { useUserContext } from '@/context/authContext'
 const Topbar = () => {
   const {mutate: signOut, isSuccess} = useSignOutAccount();
+  const {user} = useUserContext()
   const navigate = useNavigate();
 
   useEffect(()=>{
@@ -24,6 +26,9 @@ const Topbar = () => {
           >
             <img src="/assets/icons/logout.svg" alt="logout" />
           </Button>
+          <Link to={`/profile/${user.id}`} className='flex-center gap-3'>
+            <img src={user.imageUrl || '/assets/icons/profile-placeholder.svg'} alt = "profile" className='h-8 w-8 rounded-full'/>
+          </Link>
         </div>
 
       </div>
