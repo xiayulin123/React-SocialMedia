e0dcc3be6e20df94206b51d8d0b96bb5a7330eb8
xiayulin123 <xiayulinsky@gmail.com>
2024-01-03 14:14:54 -0500

    root page layout

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import BottomBar from '@/components/shared/BottomBar'
+import LeftSideBar from '@/components/shared/LeftSideBar'
+import Topbar from '@/components/shared/Topbar'
+import { Outlet } from 'react-router-dom'
 
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
+    <div className='w-full md:flex'>
+      <Topbar />
+      
+      <LeftSideBar />
+      
+      <section className=' flex flex-1 h-full'>
+        <Outlet />
+      </section>
+
+      <BottomBar />
+    </div>
   )
 }
 
---
+++ src/components/shared/LeftSideBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LeftSideBar = () => {
+  return (
+    <div>leftSideBar</div>
+  )
+}
+
+export default LeftSideBar
