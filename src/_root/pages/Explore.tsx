6cac71b6644b98dd5bd5666b683d35366ed15ce5
xiayulin123 <xiayulinsky@gmail.com>
2024-01-07 14:40:23 -0500

    fix mistakes

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -51,7 +51,7 @@ const Explore = () => {
       </div>
 
       <div className="flex flex-wrap gap-9 w-full max-w-5xl">
-        {shouldShowSearchResults ? (
+        {shouldShowSearchResults && searchedPosts ? (
           <SearchResults
             isSearchPending={isSearchPending}
             searchedPosts={searchedPosts}
@@ -60,7 +60,7 @@ const Explore = () => {
           <p className="text-light-4 mt-10 text-center w-full">End of posts</p>
         ) : (
           posts.pages.map((item, index) => (
-            <GridPostList key={`page-${index}`} posts={item.documents} />
+            <GridPostList key={`page-${index}`} posts={item!.documents} />
           ))
         )}
       </div>
